817c8af56e38a7c555f4159faf4a895a28dcbe53
amirali taghishah <amirsihezar@hotmail.com>
2024-02-17 19:42:03 +0330

    first test commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>my tic tac toe test</h1>
+    </>
+  )
+}
+
+export default App
