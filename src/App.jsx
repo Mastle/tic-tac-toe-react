eaf468a16688168366b77e6843cd09678baddcce
amirali taghishah <amirsihezar@hotmail.com>
2024-02-29 17:30:35 +0330

    lifting up the states pt.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
 import './styles.css'
 
-function Square() {
-  const [value, setValue] = useState()
+function Square({ value }) {
 
-  const handleClick = () => {
-    setValue("x")
-  }
 
-  return <button className="square" onClick={handleClick}>{value}</button>
+  return <button className="square" >{value}</button>
 }
 
 
@@ -16,25 +12,26 @@ function Square() {
 
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
 
 
   return (
     <>
       <div className="container">
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[0]} />
+          <Square value={squares[1]} />
+          <Square value={squares[2]} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[3]} />
+          <Square value={squares[4]} />
+          <Square value={squares[5]} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square value={squares[6]} />
+          <Square value={squares[7]} />
+          <Square value={squares[8]} />
         </div>
       </div>
     </>
