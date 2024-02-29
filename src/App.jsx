f617fd5afd5b7cbaec8aee48a3a546aab5dcafd0
amirali taghishah <amirsihezar@hotmail.com>
2024-02-29 16:57:49 +0330

    ui improved

--- src/App.jsx
+++ src/App.jsx
@@ -20,20 +20,22 @@ export default function Board() {
 
   return (
     <>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+      <div className="container">
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
+        <div className="board-row">
+          <Square />
+          <Square />
+          <Square />
+        </div>
       </div>
     </>
   )
